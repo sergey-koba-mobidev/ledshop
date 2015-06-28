29630145162680443ce39ba2cfeaec33700ae28a
Sergey Koba <s.koba@mobidev.biz>
2015-06-28 11:49:01 +0300

    fix state bug and add delivery instructions

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,6 +17,7 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
   config.checkout_zone = 'Ukraine'
+  config.shipping_instructions = true
 end
 
 Spree.user_class = "Spree::User"
